4d455a9d19b9dffa80f9691625ddbdaf795aa4aa
SiimonStark <justinpyktel@gmail.com>
2018-12-18 16:54:05 -0700

    Thank you Devin

--- idea.js
+++ idea.js
@@ -1,5 +1,27 @@
 class Idea {
+	constructor(id, title, body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.quality = 'swill';
+	}
 
+	saveToStorage(arrayCards){
+		//save to local storage, within a search-able array
+		localStorage.setItem("arrayCards", JSON.stringify(arrayCards));
+	}
+
+	deleteFromStorage(){
+		//delete card from storage
+	}
+
+	updateContent(){
+		//update title or body of the idea
+		//be able to edit after it has been posted
+	}
+
+	updateQuality(){
+		//be able to upvote/downvote and keep track
+		//do we need an array?
+	}
 }
-
-module.exports = Idea
